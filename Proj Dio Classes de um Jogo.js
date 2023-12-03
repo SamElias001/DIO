4b447db1a92bb68578782411be347ce00753cc6d
SamElias001 <149211876+SamElias001@users.noreply.github.com>
2023-12-03 00:48:55 -0300

    Add files via upload

---
+++ Proj Dio Classes de um Jogo.js
@@ -0,0 +1,37 @@
+class Hero {
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        this.ataque = ["magia", "espada", "artes marciais", "shuriken"];
+        this.definirAtaque()
+    }
+
+    definirAtaque() {
+        switch(this.tipo) { 
+            case "Mago":
+                this.ataque = ["magia"]
+                break
+            
+            case "Guerreiro":
+                this.ataque = ["espada"]
+                break
+
+            case "Monge":
+                this.ataque = ["artes marciais"]
+                break
+
+            default:
+                this.ataque = ["shuriken"]
+                break
+        }
+    }
+
+    ataqueDoHeroi(){
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+}
+
+let heroi = new Hero("Davies", 19, "Guerreiro")
+
+heroi.ataqueDoHeroi()
